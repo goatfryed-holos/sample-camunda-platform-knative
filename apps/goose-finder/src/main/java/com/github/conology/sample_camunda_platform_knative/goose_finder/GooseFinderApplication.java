f8d04a5e27ee76cfa2eec7387284907f018165b6
Omar Sood <omar.sood@holos-supply.de>
2024-06-19 09:28:44 +0200

    feat: add generic spring boot project goose-finder

---
+++ apps/goose-finder/src/main/java/com/github/conology/sample_camunda_platform_knative/goose_finder/GooseFinderApplication.java
@@ -0,0 +1,13 @@
+package com.github.conology.sample_camunda_platform_knative.goose_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooseFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooseFinderApplication.class, args);
+	}
+
+}
---
+++ apps/goose-finder/src/test/java/com/github/conology/sample_camunda_platform_knative/goose_finder/GooseFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.conology.sample_camunda_platform_knative.goose_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooseFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
